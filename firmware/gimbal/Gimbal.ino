338e222706ed5745d360d84f57a71a0b493e1c8d
Nativity8904 <ringork0916@gmail.com>
2025-09-07 12:17:14 -0700

    feat: barebones gimbal reaction

--- firmware/gimbal/Gimbal.ino
+++ firmware/gimbal/Gimbal.ino
@@ -1,16 +1,79 @@
+#include <Wire.h>
 #include <Servo.h>
 
+constexpr int BAUDRATE = 9600;
+constexpr int MPUADDR = 0x68;
+
+constexpr uint8_t ACCEL_CONFIG = 0x00; 
+
+constexpr float AFS_SEL = 16384.0f;
+
 Servo servo;
+constexpr int SERVO_PIN = 3;
 
 void setup() {
-  servo.attach(3);
+  // Start I2C master
+  Serial.begin(BAUDRATE);
+  Wire.begin();
+  delay(100);
 
+  // Wake MPU-6050
+  Wire.beginTransmission(MPUADDR);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission(true);
+
+  // Change accelerometer config
+  Wire.beginTransmission(MPUADDR);
+  Wire.write(0x1C);
+  Wire.write(ACCEL_CONFIG);
+  Wire.endTransmission(true);
+
+  servo.attach(SERVO_PIN);
 }
 
 void loop() {
-  servo.write(0);
-  delay(1000);
-  servo.write(180);
-  delay(1000);
 
+  // Read accel sensors
+  Wire.beginTransmission(MPUADDR);
+  Wire.write(0x3B); // Address of accel_xout_h
+  Wire.endTransmission(false);
+
+  // Request next 6 bytes from MPU (full accel values)
+  Wire.requestFrom(MPUADDR, 6);
+
+  // Read accel values from memory
+  uint8_t accel_xout_h = Wire.read(), accel_xout_l = Wire.read();
+  uint8_t accel_yout_h = Wire.read(), accel_yout_l = Wire.read();
+  uint8_t accel_zout_h = Wire.read(), accel_zout_l = Wire.read();
+
+  // Combine high and low values
+  int16_t accel_xout_raw = ((int16_t)((accel_xout_h << 8) | accel_xout_l));
+  int16_t accel_yout_raw = ((int16_t)((accel_yout_h << 8) | accel_yout_l));
+  int16_t accel_zout_raw = ((int16_t)((accel_zout_h << 8) | accel_zout_l));
+  
+  // Convert to in terms of g
+  float accel_xout = accel_xout_raw / AFS_SEL;
+  float accel_yout = accel_yout_raw / AFS_SEL;
+  float accel_zout = accel_zout_raw / AFS_SEL;
+
+  float angle = (atanf(accel_yout / accel_xout)) * (float)(180/3.1415926535);
+
+  // Convert the angle to {0, 180}
+  int servo_angle = (int)(angle) + 90;
+
+  // Print relevant values
+  Serial.print("accel_xout: ");
+  Serial.println(accel_xout);
+
+  Serial.print("accel_yout: ");
+  Serial.println(accel_yout);
+
+  Serial.print("servo_angle: ");
+  Serial.print(servo_angle);
+  Serial.print("\n");
+
+  servo.write(servo_angle);
+
+  delay(100);
 }

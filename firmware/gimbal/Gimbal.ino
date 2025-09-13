6f3d2ccbc55fb84f91e3cbc07f89d26d2a137f90
Nativity8904 <ringork0916@gmail.com>
2025-09-13 10:26:49 -0700

    refactor: replace read with `I2C_Wrapper` read

--- firmware/gimbal/Gimbal.ino
+++ firmware/gimbal/Gimbal.ino
@@ -4,25 +4,19 @@
 constexpr int BAUDRATE = 9600;
 constexpr int MPUADDR = 0x68;
 
-constexpr uint8_t ACCEL_CONFIG = 0x00; 
-
 constexpr float AFS_SEL = 16384.0f;
-
-Servo servo;
-constexpr int SERVO_PIN = 3;
-
-int servo_angle_offset;
 
 class I2C_Wrapper {
   private:
   int mpuaddr;
 
   public:
-  I2C_Wrapper(int addr) : mpuaddr(addr) {}
+  I2C_Wrapper(int address) : mpuaddr(address) {}
   
   enum MPU6050_Register : uint8_t {
     PWR_MGMT_1 = 0x6B,
-    ACCEL_CONFIG = 0x1C
+    ACCEL_CONFIG = 0x1C,
+    ACCEL_X_OUT_H = 0x3B
   };
 
   void write(uint8_t address, uint8_t data) {
@@ -32,7 +26,25 @@ class I2C_Wrapper {
     Wire.endTransmission(true);
   }
 
+  void read(uint8_t address, size_t length, uint8_t* destination) {
+    Wire.beginTransmission(mpuaddr);
+    Wire.write(address);
+    Wire.endTransmission(false);
+    Wire.requestFrom(mpuaddr, length);
+    
+    for (int n = 0; n < length; n++) {
+      destination[n] = Wire.read();
+    }
+  }
+
 };
+
+Servo servo;
+constexpr int SERVO_PIN = 3;
+
+int servo_angle_offset;
+
+uint8_t accel_raw[4]; 
 
 void setup() {
   // Start I2C master
@@ -49,29 +61,17 @@ void setup() {
 }
 
 void loop() {
+  I2C_Wrapper mpu(MPUADDR);
 
-  // Read accel sensors
-  Wire.beginTransmission(MPUADDR);
-  Wire.write(0x3B); // Address of accel_xout_h
-  Wire.endTransmission(false);
-
-  // Request next 6 bytes from MPU (full accel values)
-  Wire.requestFrom(MPUADDR, 6);
-
-  // Read accel values from memory
-  uint8_t accel_xout_h = Wire.read(), accel_xout_l = Wire.read();
-  uint8_t accel_yout_h = Wire.read(), accel_yout_l = Wire.read();
-  uint8_t accel_zout_h = Wire.read(), accel_zout_l = Wire.read();
+  mpu.read(I2C_Wrapper::ACCEL_X_OUT_H, 4, accel_raw);
 
   // Combine high and low values
-  int16_t accel_xout_raw = ((int16_t)((accel_xout_h << 8) | accel_xout_l));
-  int16_t accel_yout_raw = ((int16_t)((accel_yout_h << 8) | accel_yout_l));
-  int16_t accel_zout_raw = ((int16_t)((accel_zout_h << 8) | accel_zout_l));
+  int16_t accel_xout_raw = ((int16_t)((accel_raw[0] << 8) | accel_raw[1]));
+  int16_t accel_yout_raw = ((int16_t)((accel_raw[2] << 8) | accel_raw[3]));
   
   // Convert to in terms of g
   double accel_xout = accel_xout_raw / AFS_SEL;
   double accel_yout = accel_yout_raw / AFS_SEL;
-  float accel_zout = accel_zout_raw / AFS_SEL;
 
   float angle = (atan2f(accel_yout, accel_xout)) * (float)(180/3.1415926535);
 
